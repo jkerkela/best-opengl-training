2e9e2395584a99d100e12952448ecf933fccf2a8
Joni Kerkelä <jonikerkela@hotmail.com>
2020-06-21 14:52:13 +0300

    learnopengl Lighting maps

--- colors_shader.fs
+++ colors_shader.fs
@@ -1,31 +1,44 @@
 #version 330 core
-  
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+}; 
+
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
 
 in vec3 Normal; 
 in vec3 FragPos;
+in vec2 TexCoords;
 out vec4 FragColor;
 
-float specularStrength = 0.5;
+uniform Light light;
+uniform Material material;
+uniform vec3 viewPos;
 
 void main()
 {
-	vec3 norm = normalize(Normal);
-	vec3 lightDir = normalize(lightPos - FragPos);
-	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = diff * lightColor;
-
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
-
-	vec3 viewDir = normalize(viewPos - FragPos);
-	vec3 reflectDir = reflect(-lightDir, norm); 
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-	vec3 specular = specularStrength * spec * lightColor; 
-
-    vec3 result = (diffuse + ambient + specular) * objectColor;
-    FragColor = vec4(result, 1.0);
+	// ambient
+    vec3 ambient  = light.ambient  * vec3(texture(material.diffuse, TexCoords));
+  	
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    
+	vec3 diffuse  = light.diffuse  * diff * vec3(texture(material.diffuse, TexCoords));   
+    
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));    
+        
+    FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
